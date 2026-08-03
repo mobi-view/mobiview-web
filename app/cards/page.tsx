a0dfcb457197c0abe8a2f70d60b9846588a84930
deia <operations@mobiview.ps>
2026-08-03 14:58:33 +0300

    update program

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -6,7 +6,7 @@ export default function Card(props :any){
 
        <>
 
-        ق
+  
         <link
           href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;600;700&family=Tajawal:wght@400;500;700&display=swap"
           rel="stylesheet"
